bddcfd967f257057551fdf8efd3265e0958f8334
Samuel Lie <samuel.lie1@gmail.com>
2016-03-15 14:27:28 +0100

    Optional field only display if filled

--- inc/woocommerce/template-tags.php
+++ inc/woocommerce/template-tags.php
@@ -53,9 +53,12 @@ if ( ! function_exists( 'woocommerce_template_product_review' ) ) {
   */
 
 if ( ! function_exists( 'indielee_template_optional_field' ) ) {
+  $optional_field = html_entity_decode( get_post_meta( get_the_ID(), '_optional_field', true ) );
 
-  function indielee_template_optional_field() {
-     echo '<div class="optional-field-container">' . html_entity_decode( get_post_meta( get_the_ID(), '_optional_field', true ) ) . '</div>';
+  if ( ! ( $optional_field == null || $optional_field == '') ) {
+    function indielee_template_optional_field() {
+      echo '<div class="optional-field-container">' . $optional_field . '</div>';
+    }
   }
 
 }
