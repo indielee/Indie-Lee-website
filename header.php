faf47c1b12ea2ae8869beb896252a7f7a7123c8c
Anton Kjörck Lindén <anton@noviumdesign.se>
2016-03-25 11:48:28 +0100

    Added tag manager

--- header.php
+++ header.php
@@ -15,6 +15,16 @@ global $indielee;
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NDFFGC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NDFFGC');</script>
+<!-- End Google Tag Manager -->
+
 <?php wp_head(); ?>
 </head>
 
